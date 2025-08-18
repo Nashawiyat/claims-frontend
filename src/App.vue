24e891030b286e9f345443f88031b8e22694d396
Ahmed Al-Ansi <amedsrs10@gmail.com>
2025-08-18 11:03:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="p-6">
+    <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+      Tailwind Button
+    </button>
+  </div>
+</template>
+
+<style>
+</style>
